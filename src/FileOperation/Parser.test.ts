0345feb622274df1f1f5cfb8cf170f2871f9bfb6
IR0NSIGHT <irnsght@googlemail.com>
2023-11-28 15:22:40 +0100

    regression test that fails on invalid onlyOnLayer syntax

--- src/FileOperation/Parser.test.ts
+++ src/FileOperation/Parser.test.ts
@@ -2,6 +2,7 @@ import {getLayerById, WorldpainterApi} from "../worldpainterApi/worldpainterApi"
 import {isParsingError, parseOperations, parseTerrains} from "./Parser";
 import {parseLayers} from "./ParseLayer";
 import {Layer} from "../Layer/Layer";
+import {GeneralOperation} from "../Operation/Operation";
 
 jest.mock('../log')
 
@@ -69,7 +70,31 @@ describe("parse config", () => {
 
     const parsedOp = parseOperations(jsonString)
     expect(isParsingError(parsedOp)).toBeFalsy();
+    const opArr = (parsedOp as GeneralOperation[]);
+    expect(opArr).toHaveLength(1)
+    expect(opArr[0].filter).toHaveLength(1)
+    //TODO change filter to be able to test what kind of filter is created here. abandon OOP
+
+
 
   });
+
+  test("invalid json input:  \"onlyOnLayer\": [\"Frost\"]", ()=> {
+    const jsonString: string = `
+    {
+      "operations": [
+        {
+          "name": "Grass 1",
+          "terrain": [0],
+          "aboveLevel": -64,
+          "belowLevel": 200,
+          "aboveDegrees": 0,
+          "onlyOnLayer": ["Frost"]
+        }
+      ]
+    }
+    `
+    expect(() => parseOperations(jsonString)).toThrow("logging error!can not parse layer(s): Frost")
+  })
 
 });
